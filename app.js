4862e34defa9545d7c4672cd23f9d2dbacade26e
Malte Wirz <mail@maltewirz.com>
2020-01-28 22:38:11 +0100

    working file parser

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head>Enter message</head>');
+        res.write(` <body>
+                        <form action="/message" method="POST">
+                            <input type="text" name="message">
+                            <button type="submit">Send</button>
+                        </form>
+                    </body>`);
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                if (err) throw err;
+            });
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head>My second page');
