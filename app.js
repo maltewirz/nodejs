2bccaae7bff392fd2a08a34fb45044758028e882
Malte Wirz <mail@maltewirz.com>
2020-01-28 23:08:50 +0100

    finsihed event handling

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ const server = http.createServer((req, res) => {
         req.on('data', (chunk) => {
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFile('message.txt', message, err => {
                 if (err) throw err;
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
             });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
