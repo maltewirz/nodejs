d9da95a9f6f28deff32a9599d039fd7bb0d5e35c
Malte Wirz <mail@maltewirz.com>
2020-01-29 11:09:57 +0100

    refacoring using express

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
 
