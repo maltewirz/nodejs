21de5c5a697623c8c77a0d6f6003288f0496d52a
Malte Wirz <mail@maltewirz.com>
2020-01-30 16:54:39 +0100

    pug implementation complete

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res, next) => {    
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
