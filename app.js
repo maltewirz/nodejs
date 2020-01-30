0a417573c1d666f22746fb2bfe46d457a7d96518
Malte Wirz <mail@maltewirz.com>
2020-01-30 15:52:31 +0100

    sharing data

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const rootDir = require('./util/path');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
