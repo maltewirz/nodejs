dfd1f4c1418bdc702e31c50211ca0780d30c3a34
Malte Wirz <mail@maltewirz.com>
2020-01-30 08:04:49 +0100

    using body parser

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const http = require('http');
-
 const express = require('express');
-
 const app = express();
 
-const server = http.createServer(app);
+app.use(express.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></html>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<html>Express Server here</html>');
+});
+
+app.listen(3000,() => {
+    console.log('Server listens');
+});
 
