2c8bead778ef0b5d1fa4ebd3e069c2c31924e366
Malte Wirz <mail@maltewirz.com>
2020-01-31 17:28:20 +0100

    implementing mvc

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ app.set('views', 'views');
 const path = require('path');
 const rootDir = require('./util/path');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000,() => {
     console.log('Server listens');
