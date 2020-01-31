7d59e3a345e3429f808037cdba7fe4c8afe9a9a5
Malte Wirz <mail@maltewirz.com>
2020-01-31 18:09:33 +0100

    saving

--- models/product.js
+++ models/product.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const getProductsFromFile = (cb) => {
+    const pathDisk = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(pathDisk, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
 const products = [];
 
 module.exports = class Product {
@@ -7,9 +21,20 @@ module.exports = class Product {
 
     save() {
         products.push(this);
+        const pathDisk = path.join(rootDir, 'data', 'products.json');
+        fs.readFile(pathDisk, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(pathDisk, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 };
