00bae22b00c59875678fb98e0ba15e0192b67094
Malte Wirz <mail@maltewirz.com>
2020-02-01 07:29:14 +0100

    model view controller implementation complete

--- models/product.js
+++ models/product.js
@@ -2,17 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
 
+const pathDisk = path.join(rootDir, 'data', 'products.json');
+
 const getProductsFromFile = (cb) => {
-    const pathDisk = path.join(rootDir, 'data', 'products.json');
     fs.readFile(pathDisk, (err, fileContent) => {
         if (err) {
             cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
         }
-        cb(JSON.parse(fileContent));
     });
 };
 
-const products = [];
 
 module.exports = class Product {
     constructor(title) {
@@ -20,13 +21,7 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
-        const pathDisk = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(pathDisk, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(pathDisk, JSON.stringify(products), err => {
                 console.log(err);
