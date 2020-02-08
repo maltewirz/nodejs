5184e41081c8fbdc2320688663ad05322083a1a4
Malte Wirz <mail@maltewirz.com>
2020-02-08 06:46:53 +0100

    fetching single product

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const path = require('path');
 const rootDir = require('./util/path');
@@ -17,16 +18,21 @@ app.use(express.static(path.join(rootDir, 'public')));
 
 app.use((req, res, next) => {
     // User.findByPk(1)
-    //     .then(user => {            
+    //     .then(user => {
     //         req.user = user;
     //         next();
     //     })
     //     .catch(err => {
     //         console.log(err);
     //     });
+    next();
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+mongoConnect(() => {
+    app.listen(3000);
+});
