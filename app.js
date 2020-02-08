97e1e8ce6e3fea99b579ccfa2e76434c9cd17669
Malte Wirz <mail@maltewirz.com>
2020-02-08 08:11:30 +0100

    adding products with mongodb

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('views', 'views');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const path = require('path');
 const rootDir = require('./util/path');
@@ -17,15 +18,14 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     });
-    next();
+    User.findById('5e3e5d032310b19751db04c3')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.use('/admin', adminRoutes);
@@ -34,5 +34,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
+    
     app.listen(3000);
 });
