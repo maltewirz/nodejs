ce777b8f084aa558e080da2d4a6806ce84928c24
Malte Wirz <mail@maltewirz.com>
2020-02-12 07:21:23 +0100

    password working

--- app.js
+++ app.js
@@ -56,18 +56,6 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  })
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'cat',
-                    email: 'test@example.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(3000);
     })
     .catch(err => console.log(err));
