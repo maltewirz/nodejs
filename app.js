4b1afed3708e5aa693a5d52d897ba0b8d95b6e13
Malte Wirz <mail@maltewirz.com>
2020-02-12 08:13:52 +0100

    authentication done

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const dbCredentials = require('./util/dbCredentials');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash =  require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +18,7 @@ const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -32,6 +35,8 @@ app.use(session({
     saveUninitialized: false,
     store: store
 }));
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -45,6 +50,12 @@ app.use((req, res, next) => {
         .catch(err => {
             console.log(err);
         });
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use('/admin', adminRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,31 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
-exports.getLogin = (req, res, next) => {          
+exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login', 
-        isAuthenticated: req.session.isLoggedIn
+        errorMessage: message
     });
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
         path: '/signup',
-        pageTitle: 'Signup',
-        isAuthenticated: false
+        pageTitle: 'Signup', 
+        errorMessage: message
     });
 };
 
@@ -24,6 +36,7 @@ exports.postLogin = (req, res, next) => {
     User.findOne({ email: email })
         .then(user => {
             if (!user) {
+                req.flash('error', 'invalid email or password');
                 return res.redirect('/login');
             }
             bcrypt.compare(password, user.password)
@@ -40,6 +53,7 @@ exports.postLogin = (req, res, next) => {
                 })
                 .catch(err => {
                     console.log(err);
+                    req.flash('error', 'invalid email or password');
                     res.redirect('login');
                 });
         })
@@ -56,6 +70,7 @@ exports.postSignup = (req, res, next) => {
     User.findOne({email: email})
         .then(userDoc => {
             if (userDoc) {
+                req.flash('error', 'Email exists already.');
                 return res.redirect('/signup');
             }
             return bcrypt
