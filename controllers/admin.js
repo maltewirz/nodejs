fa9eedcbe2ce25eddf34d6e72ea16a4c50d6210e
Malte Wirz <mail@maltewirz.com>
2020-02-13 07:30:53 +0100

    authentication and authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -66,15 +66,19 @@ exports.postEditProduct = (req, res, next) => {
     Product
         .findById(prodId)
         .then(product => {
+            if (product.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
             product.imageUrl = updatedImageUrl;
-            return product.save();
-        })
-        .then(result => {
-            console.log('Updated product');
-            res.redirect('/admin/products');
+            return product
+                .save()
+                .then(result => {
+                    console.log('Updated product');
+                    res.redirect('/admin/products');
+                });
         })
         .catch(err => {
             console.log(err);
@@ -83,7 +87,11 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.deleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product
+        .deleteOne({
+            _id: prodId,
+            userId: req.user._id
+        })
         .then(result => {
             console.log('Destroyed Product');
             res.redirect('/admin/products');
@@ -93,8 +101,11 @@ exports.deleteProduct = (req, res, next) => {
         });
 };
 
-exports.getProducts = (req, res, next) => {    
-    Product.find()
+exports.getProducts = (req, res, next) => {
+    Product
+        .find({
+            userId: req.user._id
+        })
         .then(products => {
             console.log(products);
             res.render('admin/products', {
