b4a0932ef9e8da5765f0a3d904d77415618afadf
Malte Wirz <mail@maltewirz.com>
2020-02-14 07:34:15 +0100

    validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,7 +36,13 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup', 
-        errorMessage: message
+        errorMessage: message,
+        oldInput: { 
+            email: '', 
+            password: '', 
+            confirmPassword: ''
+        },
+        validationErrors: []
     });
 };
 
@@ -89,7 +95,13 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup', 
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput: { 
+                email: email, 
+                password: password, 
+                confirmPassword: req.body.confirmPassword
+            },
+            validationErrors: errors.array()
         });
     }
     bcrypt
