af55cf02e7dc565ce91be7c374a456a37e958f23
Malte Wirz <mail@maltewirz.com>
2020-02-17 10:25:25 +0100

    added client side code for handling async js

--- controllers/admin.js
+++ controllers/admin.js
@@ -152,31 +152,6 @@ exports.postEditProduct = (req, res, next) => {
         });
 };
 
-exports.deleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product
-        .findById(prodId)
-        .then(product => {
-            if (!product) {
-                return next(new Error('Product not found'));
-            }
-            fileHelper.deleteFile(product.imageUrl);
-            return Product
-                .deleteOne({
-                    _id: prodId,
-                    userId: req.user._id
-                });
-        })
-        .then(() => {
-            res.redirect('/admin/products');
-        })
-        .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
-        });
-};
-
 exports.getProducts = (req, res, next) => {
     Product
         .find({
@@ -193,5 +168,28 @@ exports.getProducts = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
+        });
+};
+
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product
+        .findById(prodId)
+        .then(product => {
+            if (!product) {
+                return next(new Error('Product not found'));
+            }
+            fileHelper.deleteFile(product.imageUrl);
+            return Product
+                .deleteOne({
+                    _id: prodId,
+                    userId: req.user._id
+                });
+        })
+        .then(() => {
+            res.status(200).json({message: 'Success!'});
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Deleting product failed.'});
         });
 };
