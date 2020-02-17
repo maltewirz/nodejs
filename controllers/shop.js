8ac760e2cde69d19f2c58949ff4ec3972e81dee0
Malte Wirz <mail@maltewirz.com>
2020-02-17 07:32:51 +0100

    pagination progress

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
     Product
@@ -44,18 +44,35 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    const page = req.query.page;
-    Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE) 
-        .limit(ITEMS_PER_PAGE)
-        .then(products => {
+    const page = +req.query.page || 1;    
+    let totalItems;
+
+    Product
+        .find()
+        .countDocuments()
+        .then(numProducts => {            
+            totalItems = numProducts;
+            return Product
+                .find()
+                .skip((page - 1) * ITEMS_PER_PAGE) 
+                .limit(ITEMS_PER_PAGE)
+        })
+        .then(products => {            
             res.render('shop/index', {
                 prods: products, 
                 pageTitle: 'Shop', 
-                path: '/'
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
+            console.log(err);
+            
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
